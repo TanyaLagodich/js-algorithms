4b5a7b195bcd11f3cb19f960b0e626897c41a16b
Tanya Lagodich <tanyalagodichka@gmail.com>
2020-06-14 20:26:56 +0300

    add insertion sort by ascending && descending

--- insertionSort.js
+++ insertionSort.js
@@ -1,8 +1,22 @@
-const insertionSort = (arr) => {
+export const insertionSortByAscending = (arr) => {
     for (let i = 0; i < arr.length; i++) {
         const current = arr[i];
         let j = i;
         while(j > 0 && arr[j - 1] > current) {
+            arr[j] = arr[j - 1];
+            j--;
+        }
+        arr[j] = current;
+    }
+    return arr;
+}
+
+export const insertionSortByDescending = (arr) => {
+    for (let i = 1; i < arr.length; i++) {
+        const current = arr[i];
+        let j = i;
+
+        while(j > 0 && arr[j - 1] < current) {
             arr[j] = arr[j - 1];
             j--;
         }
